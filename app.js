15ced74b1dfa8f9d2ae156ac2ebe76e0277e0ee6
ndoelger <doelgern@gmail.com>
2024-05-10 20:00:47 -0500

    successfully retrieved access token

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const PORT = 3001;
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const axios = require("axios");
 
 const app = express();
 
@@ -16,16 +17,44 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
 
-app.get("/install", function (req, res) {
-  console.log(CLIENT_ID);
+app.get("/", (req, res) => {
   const authURL = `https://app.hubspot.com/oauth/authorize?client_id=${encodeURIComponent(
     CLIENT_ID
   )}&scope=${encodeURIComponent([
     "crm.objects.contacts.read",
   ])}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}`;
 
-  console.log(authURL);
   res.redirect(authURL);
+});
+
+// Callback
+app.get("/oauth-callback", async (req, res) => {
+  if (req.query.code) {
+    const params = new URLSearchParams({
+      grant_type: "authorization_code",
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      redirect_uri: REDIRECT_URI,
+      code: req.query.code,
+    });
+
+    try {
+      const response = await axios.post(
+        "https://api.hubapi.com/oauth/v1/token",
+        params.toString(), // Converts the parameters to URL-encoded string
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+
+      res.send(response.data); // Sends the tokens as response
+    } catch (error) {
+      console.error("Error:", error.response.data); // More detailed error logging
+      res.status(500).send("Failed to retrieve access token");
+    }
+  }
 });
 
 app.listen(PORT, () => console.log(`Listening on PORT:${PORT}`));
