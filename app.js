ef5b2605f2eb1c54a5af8c8c9180da12b89cd061
ndoelger <doelgern@gmail.com>
2024-05-12 11:45:23 -0400

    set refresh token

--- app.js
+++ app.js
@@ -14,14 +14,18 @@ const session = require("express-session");
 
 const app = express();
 
-let refreshToken;
+let refreshToken = {};
 
 const tokenCache = require("./cache");
-
-const userRouter = require("./user/userRouter");
+const {
+  getAuthorization,
+  getAccessToken,
+  exchangeForTokens,
+} = require("./user/userController");
 
 app.use(express.json());
 app.use(cors());
+
 app.use(express.static("public"));
 
 app.use(
@@ -32,7 +36,73 @@ app.use(
   })
 );
 
-// app.get('/')
-app.use("/user", userRouter);
+app.get("/login", (req, res) => {
+  const authUrl =
+    "https://app.hubspot.com/oauth/authorize" +
+    `?client_id=${encodeURIComponent(CLIENT_ID)}` + // app's client ID
+    `&scope=${encodeURIComponent(SCOPES)}` + // scopes being requested by the app
+    `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}`; // where to send the user after the consent page
+
+  res.redirect(authUrl);
+});
+
+app.get("/callback", async (req, res) => {
+  const params = new URLSearchParams({
+    grant_type: "authorization_code",
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    redirect_uri: REDIRECT_URI,
+    code: req.query.code,
+  });
+
+  const tokens = await exchangeForTokens(req.sessionID, params);
+  refreshToken[req.sessionID] = tokens.refresh_token;
+  console.log(refreshToken);
+  // Once the tokens have been retrieved, use them to make a query
+  // to the HubSpot API
+  res.redirect(`/`);
+});
+
+app.get("/", async (req, res) => {
+  console.log(refreshToken);
+  if (refreshToken[req.sessionID]) {
+    res.redirect("/dashboard");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/contacts", async (req, res) => {
+  const accessToken = await getAccessToken(req.sessionID);
+  console.log(accessToken);
+  try {
+    const response = await axios.get(
+      `https://api.hubapi.com/crm/v3/objects/contacts/?limit=10&properties=email,firstname,lastname,jobtitle,company`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const contacts = response.data.results.map((contact) => {
+      return {
+        id: contact.id,
+        firstname: contact.properties.firstname || "N/A", // Safe access
+        lastname: contact.properties.lastname || "N/A", // Safe access
+        email: contact.properties.email || "N/A",
+        // createdAt: dateCoverter(contact.createdAt),
+        company: contact.properties.company || "N/A",
+        jobtitle: contact.properties.jobtitle || "N/A",
+      };
+    });
+    if (response.data.paging) contacts.after = response.data.paging.next.after;
+
+    res.json(contacts); // Send contacts array as JSON response
+  } catch (error) {
+    console.error("Error:", error.response ? error.response.data : error);
+    res.status(500).json({ error: "Failed to fetch contacts" }); // Send error response
+  }
+});
 
 app.listen(PORT, () => console.log(`Listening on PORT:${PORT}`));
--- cache.js
+++ cache.js
@@ -1,4 +1,5 @@
 const NodeCache = require("node-cache");
 const tokenCache = new NodeCache({ deleteOnExpire: true });
+const refreshToken = {};
 
 module.exports = tokenCache;
