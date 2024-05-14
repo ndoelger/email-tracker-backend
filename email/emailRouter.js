1ab9a1fe8af5797b1d183bf7e6780b6e8ac373ba
ndoelger <doelgern@gmail.com>
2024-05-13 20:03:56 -0400

    successful edit

--- email/emailRouter.js
+++ email/emailRouter.js
@@ -5,6 +5,7 @@ const emailCtrl = require("./emailController");
 router.get("/", emailCtrl.getEmails);
 router.post("/add", emailCtrl.addEmail);
 router.delete("/:id", emailCtrl.deleteEmail);
+router.patch("/:id", emailCtrl.editEmail);
 
 
 module.exports = router;
